fd502b4ff8a725b1402643139db4ad27c86c2811
Nimrod Flaschner <nimrodifla@gmail.com>
2023-11-21 18:46:13 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const PORT = 80;
+
+app.use(express.json()); 
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/timeline.html');
+});
+
+app.get('/data', (req, res)=>{
+    res.sendFile(__dirname + '/data.json');
+});
+
+app.post('/addEvt', (req, res)=>{
+    console.log(req.body);
+    let name = req.body.name;
+    let desc = req.body.desc;
+    let date = req.body.date;
+
+    let valid = true;
+    // make sure data's format is valid
+    if (name.length <= 0 || desc.length <= 0 || Date(date) == undefined) {
+        valid = false;
+    }
+
+    let added = false;
+    if (valid) {
+        // ## add event
+
+        let newEvt = {"date": date, "value": name, "desc": desc};
+        // update file
+        let data = JSON.parse(fs.readFileSync(__dirname + '/data.json', 'utf8'));
+        console.log(data);
+        data['events'].push(newEvt);
+        fs.writeFileSync(__dirname + '/data.json', JSON.stringify(data));
+
+        added = true;
+    }
+
+    // send OK
+    res.send({'added': added});
+});
+
+app.listen(PORT, (err) => {
+    if (err)
+        throw err;
+    
+    console.log('listening on port: ' + PORT);
+});
